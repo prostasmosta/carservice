a841443dc26988c8da187751834d1904673100f4
Slava Garipov <prostasmosta@gmail.com>
2022-07-08 12:07:05 +0300

    Actions fixes

--- app/controllers/service_categories_controller.rb
+++ app/controllers/service_categories_controller.rb
@@ -6,29 +6,29 @@ class ServiceCategoriesController < ApplicationController
   end
 
   def create
-    @service_category = ServiceCategory.new
+    @service_category = ServiceCategory.new(cat_params)
 
     if @service_category.save
-      redirect_to service_category_path, notice: 'Новый заказ создан!'
+      redirect_to @service_category, notice: 'Новая категория создана!'
     else
-      flash.now[:alert] = 'При попытке создать заказ возникли ошибки'
+      flash.now[:alert] = 'При попытке создать категорию возникли ошибки'
 
       render :new
     end
   end
 
   def update
-    if @service_category.save
-      redirect_to service_category_path, notice: 'Заказ обновлен!'
+    if @service_category.update(cat_params)
+      redirect_to @service_category, notice: 'Категория обновлена!'
     else
-      flash.now[:alert] = 'При попытке обновить заказ возникли ошибки'
+      flash.now[:alert] = 'При попытке обновить категорию возникли ошибки'
 
       render :edit
     end
   end
 
   def show
-    @service_category = ServiceCategory.find(params[:id])
+    @services = @service_category.services.order(created_at: :desc)
   end
 
   def index
@@ -36,15 +36,18 @@ class ServiceCategoriesController < ApplicationController
   end
 
   def destroy
-    @user = @question.user
-    @question.destroy
+    @service_category.destroy
 
-    redirect_to user_path(@user), notice: 'Вопрос удален!'
+    redirect_to service_categories_path, notice: 'Категория удалена!'
   end
 
   private
 
   def set_cat
     @service_category = ServiceCategory.find(params[:id])
+  end
+
+  def cat_params
+    params.require(:service_category).permit(:title)
   end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,54 +1,63 @@
 class ServicesController < ApplicationController
-  before_action :set_service, only: %i[show update edit destroy]
+  before_action :set_cat, except: %i[]
+  before_action :set_service, only: %i[new show update edit destroy]
 
   def new
+    # @service = @service_category.services.build
     @service = Service.new
+    @service.service_category_id = params[:service_category_id]
   end
 
   def create
-    @service = Service.new(service_params)
+    @service = @service_category.services.build(service_params)
 
     if @service.save
-      redirect_to service_path, notice: 'Новый заказ создан!'
+      redirect_to @service_category, notice: 'Новая услуга добавлена!'
     else
-      flash.now[:alert] = 'При попытке создать заказ возникли ошибки'
+      flash.now[:alert] = 'При попытке добавить услугу возникли ошибки'
 
       render :new
     end
   end
 
   def update
-    if @service.save
-      redirect_to service_path, notice: 'Заказ обновлен!'
+    if @service.update(service_params)
+      redirect_to @service_category, notice: 'Услуга обновлена!'
     else
-      flash.now[:alert] = 'При попытке обновить заказ возникли ошибки'
+      flash.now[:alert] = 'При попытке обновить услугу возникли ошибки'
 
       render :edit
     end
   end
 
+  def edit
+  end
+
   def show
-    @service = Service.find(params[:id])
   end
 
   def index
-    @services = Service.order(created_at: :desc)
+    # @service_category = ServiceCategory.find(params[:service_category_id])
+    @services = @service_category.services.order(created_at: :desc)
   end
 
   def destroy
-    @user = @question.user
-    @question.destroy
+    @service.destroy
 
-    redirect_to user_path(@user), notice: 'Вопрос удален!'
+    redirect_to @service_category, notice: 'Услуга удалена!'
   end
 
   private
 
+  def set_cat
+    @service_category = ServiceCategory.find(params[:id])
+  end
+
   def set_service
-    @service = Service.find(params[:id])
+    @service = @service_category.services.find(params[:service_category_id])
   end
 
   def service_params
-    params.require(:service).permit(:title)
+    params.require(:service).permit(:title, :service_category_id)
   end
 end
