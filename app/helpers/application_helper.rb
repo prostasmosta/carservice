c6efe3f99b492d4405d0b49680048b93f38b2cb6
Slava Garipov <prostasmosta@gmail.com>
2022-07-21 14:37:52 +0300

    Add helper for sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,10 @@ module ApplicationHelper
     end
     nil
   end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}
+  end
 end
