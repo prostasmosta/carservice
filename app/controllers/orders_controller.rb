89a75d0fe4186ee55e9971d429e18187ef596a49
Slava Garipov <prostasmosta@gmail.com>
2022-07-21 15:10:36 +0300

    Check heroku fails v2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,6 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.joins(:services, :executors).
       order(sort_column + " " + sort_direction).
-      uniq.
       paginate(page: params[:page], per_page: 8)
 
     @orders = @orders.find_by_customer_name(params[:customer_name]) if params[:customer_name].present?
